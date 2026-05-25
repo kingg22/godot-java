c3f2dfd8ce06ab9ee4d67ecc245c51c23a155a02
Rey <89825425+kingg22@users.noreply.github.com>
2026-05-24 20:22:23 -0500

    perf(native, ffi): optimize UByte constants with inline

--- kotlin-native/ffi/src/nativeMain/kotlin/io/github/kingg22/godot/internal/ffi/Utils.kt
+++ kotlin-native/ffi/src/nativeMain/kotlin/io/github/kingg22/godot/internal/ffi/Utils.kt
@@ -1,5 +1,5 @@
 package io.github.kingg22.godot.internal.ffi
 
-public val UByte.Companion.TRUE: GDExtensionBool get() = 1u
+public val UByte.Companion.TRUE: GDExtensionBool inline get() = 1u
 
-public val UByte.Companion.FALSE: GDExtensionBool get() = 0u
+public val UByte.Companion.FALSE: GDExtensionBool inline get() = 0u
